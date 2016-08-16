40f036c72bc942c19c5920dd46c2e1e1ddb50048
Nathan Francy <nathanfrancy@gmail.com>
2016-08-15 20:11:21 -0500

    Adding ability to read specific key value out of contents of encrypted file.

--- test/test.js
+++ test/test.js
@@ -4,4 +4,9 @@ safetext.init('password', { hello: 'world' }).then(function() {
     return safetext.getContents('password');
 }).then(function(contents) {
     console.log(contents);
+    return safetext.getKey('hello', 'password');
+}).then(function(value) {
+    console.log(value);
+}).catch(function(err) {
+    console.log(err);
 });
