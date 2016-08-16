883e5280849161e297c0aba5057d26167e8fba5c
Nathan Francy <nathanfrancy@gmail.com>
2016-08-15 21:19:48 -0500

    Adding basic level command line support to what's supported in the api so far.

--- test/test.js
+++ test/test.js
@@ -7,6 +7,11 @@ safetext.init('password', { hello: 'world' }).then(function() {
     return safetext.getKey('hello', 'password');
 }).then(function(value) {
     console.log(value);
+    return safetext.writeKey('password', 'password', 'password');
+}).then(function() {
+    return safetext.getContents('password');
+}).then(function(contents) {
+    console.log(contents);
 }).catch(function(err) {
     console.log(err);
 });
